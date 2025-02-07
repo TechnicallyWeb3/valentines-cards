a830e629aba11768dff2c30b021339ff555441d8
Technically Web3 <mancinoweb3@gmail.com>
2025-02-07 02:41:24 -0700

    Profile fix

--- public/script.js
+++ public/script.js
@@ -18,29 +18,29 @@ const profiles = [
         address: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
     },
     {
-        name: "Satoshi Nakamoto",
-        image: "path/to/satoshi.jpg",
-        address: "0x0000000000000000000000000000000000000000"
+        name: "Gary Veynerchuck",
+        image: "https://i.seadn.io/gae/YX3nBiTQ1N7Pa6ymJTD2I9ihuxiwVY-gvBloyt5vf8QMitXYKX_KEdf7FyfTGaD9BObTmO6E4OzDUKrsli0w8B7xRc-jqJnqhIxu5Q?auto=format&dpr=1&w=256",
+        address: "0x5ea9681c3ab9b5739810f8b91ae65ec47de62119"
     },
     {
-        name: "Vitalik Buterin",
-        image: "https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg",
-        address: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+        name: "Donald Trump",
+        image: "https://www.whitehouse.gov/wp-content/uploads/2025/01/Donald-J-Trump.jpg",
+        address: "0x94845333028B1204Fbe14E1278Fd4Adde46B22ce"
     },
     {
-        name: "Satoshi Nakamoto",
-        image: "path/to/satoshi.jpg",
-        address: "0x0000000000000000000000000000000000000000"
+        name: "Paris Hilton",
+        image: "https://i.seadn.io/gcs/files/4351d491a6e60dc3915d555762e5dadb.jpg?auto=format&dpr=1&w=256",
+        address: "0xB6Aa5a1AA37a4195725cDF1576dc741d359b56bd"
     },
     {
-        name: "Vitalik Buterin",
-        image: "https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg",
-        address: "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"
+        name: "Mark Cuban",
+        image: "https://i.seadn.io/gae/yMiURhMqqYzfoZZUz09s1fwZqBRfQylXwN8jIYjKNpy3ClCPNCJIjJA-BuI3M6QvO6cMED9ag9l9MSow4ohiag55pUE_D_0GzBzxlA?auto=format&dpr=1&w=256",
+        address: "0xa679c6154b8d4619Af9F83f0bF9a13A680e01eCf"
     },
     {
-        name: "Satoshi Nakamoto",
-        image: "path/to/satoshi.jpg",
-        address: "0x0000000000000000000000000000000000000000"
+        name: "Steve Aoki",
+        image: "https://i.seadn.io/gae/FDYglkKVkwubS6YrgjWa8Nqa6E47sccB41Va7u0OlvmQwUiOrKiCund13JVSXzLZx76ms--QcVgonfqCbMEBuUMTDmSy9mWsRt-d?auto=format&dpr=1&w=256",
+        address: "0xe4bBCbFf51e61D0D95FcC5016609aC8354B177C4"
     },
     // Add more profiles as needed
 ];
@@ -759,7 +759,7 @@ function initializeCarousel() {
         card.className = 'profile-card';
         card.innerHTML = `
             <div class="profile-image-container">
-                <img src="${profile.image}" alt="${profile.name}" class="profile-image">
+                <img src="${profile.image}" alt="${profile.name}" class="profile-image" height="100%" width="100%">
             </div>
             <div class="profile-info">
                 <div class="profile-name">${profile.name}</div>
